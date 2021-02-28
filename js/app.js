d8980ebee592ee51bdd0abde58b693b8c1befd5f
namura <offdek555@gmail.com>
2021-02-28 16:41:38 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -6,9 +6,9 @@ const sunSplash = document.querySelector(".sun-splash");
 body.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
   smoke.style.transform = `translate(${xAxis * 45}px)`;
-  sunSplash.style.transform = `rotate(${xAxis * 1.25}deg)`;
+  sunSplash.style.transform = `rotate(${xAxis}deg)`;
   if (xAxis < 0) {
-    sunSplash.style.top = `${xAxis * 17}px`;
+    sunSplash.style.top = `${xAxis * 15}px`;
   } else {
     sunSplash.style.top = `0`;
   }
