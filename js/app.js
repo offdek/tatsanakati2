8477ffa96eb1fa6c75806a8f9e6abd5dfd8110d5
namura <offdek555@gmail.com>
2021-03-01 00:09:22 +0700

    update css and app.js use sensor mobile

--- js/app.js
+++ js/app.js
@@ -3,16 +3,35 @@ const body = document.querySelector("body");
 const smoke = document.querySelector(".smoke");
 const sunSplash = document.querySelector(".sun-splash");
 
+// desktop
 const speedSmoke = 45;
-const rotateSunSplash = 15;
+const rotateSunSplashRate = 15;
+
+//mobile
+const waitRollingRate = 40;
 
 body.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
   smoke.style.transform = `translate(${xAxis * speedSmoke}px)`;
   sunSplash.style.transform = `rotate(${xAxis}deg)`;
+  console.log(xAxis);
   if (xAxis < 0) {
-    sunSplash.style.top = `${xAxis * rotateSunSplash}px`;
+    sunSplash.style.top = `${xAxis * rotateSunSplashRate}px`;
   } else {
-    sunSplash.style.top = `${xAxis * -rotateSunSplash}px`;
+    sunSplash.style.top = `${xAxis * -rotateSunSplashRate}px`;
   }
 });
+
+if (window.DeviceOrientationEvent) {
+  window.addEventListener("deviceorientation", (e) => {
+    smoke.style.transform = `translate(${e.alpha}px)`;
+    sunSplash.style.transform = `rotate(${e.alpha / waitRollingRate}deg)`;
+    if (e.alpha < 0) {
+      sunSplash.style.left = `${e.alpha / -12}px`;
+      sunSplash.style.top = `${e.alpha / 6}px`;
+    } else {
+      sunSplash.style.left = `${e.alpha / -12}px`;
+      sunSplash.style.top = `${e.alpha / -6}px`;
+    }
+  });
+}
