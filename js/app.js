d432cb840c4a4023022703324e1c323f67d0b1b4
namura <offdek555@gmail.com>
2021-02-28 16:31:10 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+//Movement Animation
+const body = document.querySelector("body");
+const smoke = document.querySelector(".smoke");
+const sunSplash = document.querySelector(".sun-splash");
+
+body.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
+  smoke.style.transform = `translate(${xAxis * 45}px)`;
+  sunSplash.style.transform = `rotate(${xAxis}deg)`;
+  if (xAxis < 0) {
+    sunSplash.style.top = `${xAxis * 15}px`;
+  } else {
+    sunSplash.style.top = `0`;
+  }
+});
