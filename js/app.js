c34d3a1b25d718bfc0e786294733fef4f9458db3
namura <offdek555@gmail.com>
2021-03-01 00:47:17 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -7,9 +7,6 @@ const sunSplash = document.querySelector(".sun-splash");
 const speedSmoke = 45;
 const rotateSunSplashRate = 15;
 
-//mobile
-const waitRollingRate = 40;
-
 body.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
   smoke.style.transform = `translate(${xAxis * speedSmoke}px)`;
@@ -20,6 +17,9 @@ body.addEventListener("mousemove", (e) => {
     sunSplash.style.top = `${xAxis * -rotateSunSplashRate}px`;
   }
 });
+
+//mobile
+const waitRollingRate = 40;
 
 if (window.DeviceOrientationEvent) {
   window.addEventListener("deviceorientation", (e) => {
