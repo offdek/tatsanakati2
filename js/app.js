ea5e0ebbaddc2eccc38a8a33bb2fdc765f36ed07
namura <offdek555@gmail.com>
2021-03-02 23:15:02 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,25 @@
 //Movement Animation
-const body = document.querySelector("body");
-const smoke = document.querySelector(".smoke");
-const sunSplash = document.querySelector(".sun-splash");
+var body = document.querySelector("body");
+var smoke = document.querySelector(".smoke");
+var sunSplash = document.querySelector(".sun-splash");
 
 // desktop
-const speedSmoke = 45;
-const rotateSunSplashRate = 15;
+var speedSmoke = 15;
+var rotateSunSplashRate = 7;
 
 body.addEventListener("mousemove", (e) => {
   let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
-  smoke.style.transform = `translate(${xAxis * speedSmoke}px)`;
-  sunSplash.style.transform = `rotate(${xAxis}deg)`;
-  if (xAxis < 0) {
-    sunSplash.style.top = `${xAxis * rotateSunSplashRate}px`;
-  } else {
-    sunSplash.style.top = `${xAxis * -rotateSunSplashRate}px`;
+  smoke.style.transform = `translate(${xAxis * -speedSmoke}px)`;
+  sunSplash.style.transform = `translateX(${xAxis * -rotateSunSplashRate}px)`;
+});
+// mobile;
+var waitRollingRate = 10;
+
+window.addEventListener("load", () => {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener("deviceorientation", (e) => {
+      smoke.style.transform = `translate(${e.alpha}px)`;
+      sunSplash.style.transform = `translateX(${e.alpha / waitRollingRate}px)`;
+    });
   }
 });
-
-//mobile
-const waitRollingRate = 40;
-
-if (window.DeviceOrientationEvent) {
-  window.addEventListener("deviceorientation", (e) => {
-    smoke.style.transform = `translate(${e.alpha}px)`;
-    sunSplash.style.transform = `rotate(${e.alpha / waitRollingRate}deg)`;
-    if (e.alpha < 0) {
-      sunSplash.style.left = `${e.alpha / -12}px`;
-      sunSplash.style.top = `${e.alpha / 6}px`;
-    } else {
-      sunSplash.style.left = `${e.alpha / -12}px`;
-      sunSplash.style.top = `${e.alpha / -6}px`;
-    }
-  });
-}
