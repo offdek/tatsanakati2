1a810efabdc12a744df0dc8a4df8cf15d679eef6
namura <offdek555@gmail.com>
2021-03-05 16:26:37 +0700

    update ios motion

--- js/app.js
+++ js/app.js
@@ -14,41 +14,45 @@ body.addEventListener("mousemove", (e) => {
 });
 // mobile;
 const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
+let px = 45; // Position x and y
+let vx = 0; // Velocity x and y
+const updateRate = 1 / 32;
 // Windows Phone must come first because its UA also contains "Android"
 if (/windows phone/i.test(userAgent)) {
-  alert("Windows Phone");
+  alert("Windows Phone not Subport Motion");
+  btn.style.display = "none";
 }
 
 if (/android/i.test(userAgent)) {
-  alert("Android");
+  btn.style.display = "none";
+  Orientation();
 }
 
 // iOS detection from: http://stackoverflow.com/a/9039885/177710
 if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
   btn.style.display = "block";
+  // IOS Function
+  function getAccel() {
+    DeviceMotionEvent.requestPermission().then((response) => {
+      if (response == "granted") {
+        btn.style.display = "none";
+        Orientation();
+      }
+    });
+  }
 }
 
-// IOS
-function getAccel() {
-  DeviceMotionEvent.requestPermission().then((response) => {
-    if (response == "granted") {
-      btn.style.display = "none";
-      let px = 45; // Position x and y
-      let vx = 0; // Velocity x and y
-      const updateRate = 1 / 60;
-      window.addEventListener("deviceorientation", (e) => {
-        const leftToRight = e.gamma;
-        vx = vx + leftToRight * updateRate;
-        px = px + vx * 0.5;
-        console.log(px);
-        if (px > 98 || px < 0) {
-          px = Math.max(0, Math.min(98, px));
-          vx = 0;
-        }
-        smoke.style.transform = `translateX(${px / 3}px)`;
-        sunSplash.style.transform = `translateX(${px / 3}px)`;
-      });
+function Orientation() {
+  window.addEventListener("deviceorientation", (e) => {
+    const leftToRight = e.gamma;
+    vx = vx + leftToRight * updateRate;
+    px = px + vx * 0.5;
+    console.log(px);
+    if (px > 98 || px < 0) {
+      px = Math.max(0, Math.min(98, px));
+      vx = 0;
     }
+    smoke.style.transform = `translateX(${px / 1.5}px)`;
+    sunSplash.style.transform = `translateX(${px / 3}px)`;
   });
 }
