3417af822a17ce63866e7b73c28f88c1e6029e89
namura <offdek555@gmail.com>
2021-03-05 18:39:42 +0700

    update app.js

--- js/app.js
+++ js/app.js
@@ -3,33 +3,31 @@ const body = document.querySelector("body");
 const smoke = document.querySelector(".smoke");
 const sunSplash = document.querySelector(".sun-splash");
 const btn = document.querySelector(".btn");
+const isMobile = navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)/i);
 // desktop
 const speedSmoke = 15;
 const rotateSunSplashRate = 7;
 
-body.addEventListener("mousemove", (e) => {
-  let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
-  smoke.style.transform = `translateX(${xAxis * -speedSmoke}px)`;
-  sunSplash.style.transform = `translateX(${xAxis * -rotateSunSplashRate}px)`;
-});
-// mobile;
-const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-let px = 0; // Position x and y
-let vx = 0; // Velocity x and y
-const updateRate = 1 / 20;
-// Windows Phone must come first because its UA also contains "Android"
-if (/windows phone/i.test(userAgent)) {
-  alert("Windows Phone not Subport Motion");
-  btn.style.display = "none";
+if (isMobile == null) {
+  body.addEventListener("mousemove", (e) => {
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 270;
+    smoke.style.transform = `translateX(${xAxis * -speedSmoke}px)`;
+    sunSplash.style.transform = `translateX(${xAxis * -rotateSunSplashRate}px)`;
+  });
 }
 
-if (/android/i.test(userAgent)) {
+// mobile;
+let px = 0; // Position x and y
+let vx = 0; // Velocity x and y
+
+const updateRate = 1 / 45; // หน่วงการเคลื่อนที่
+
+Orientation();
+if (/android/i.test(isMobile)) {
   btn.style.display = "none";
   Orientation();
 }
-
-// iOS detection from: http://stackoverflow.com/a/9039885/177710
-if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+if (/iPad|iPhone|iPod/.test(isMobile) && !window.MSStream) {
   btn.style.display = "block";
   // IOS Function
   function getAccel() {
@@ -46,10 +44,11 @@ function Orientation() {
   window.addEventListener("deviceorientation", (e) => {
     const leftToRight = e.gamma;
     vx = vx + leftToRight * updateRate;
+    console.log(vx);
     px = px + vx / 4;
-    console.log(px);
-    if (px > 100 || px < -100) {
-      px = Math.max(-100, Math.min(100, px));
+    if (px > 120 || px < -120) {
+      px = Math.max(-120, Math.min(120, px));
+      console.log(px);
       vx = 0;
     }
     smoke.style.transform = `translateX(${px / 1.5}px)`;
